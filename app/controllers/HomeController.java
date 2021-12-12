04540902cd08f37b69ab859d4bf604c20589eaec
yoshidanobutaka <nobutaka.yoshida@rakus-partners.co.jp>
2021-12-12 11:53:21 +0900

    ok function

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import play.mvc.*;
+/** ---Resultクラスを使う際、追加でインポート--- */
 import akka.util.*;
 import java.util.*;
 import play.http.*;
@@ -54,14 +56,24 @@ public class HomeController extends Controller {
      *   ・（第１引数）ByteString　＝　実際にコンテンツとして出力されるテキスト
      *   ・（第２引数）Optional　＝　テキストの値を指定
      */
+//    public Result index() {
+//        return new Result(
+//                new ResponsiHeader(200, new HashMap<>),
+//                new HttpEntity.Strict(
+//                        ByteString.fromString("This is sample text."),
+//                        Optional.ofNullable("text/plain")
+//                )
+//        );
+//    }
+
+
+    /**
+     * Ok関数の中身でhtmlタグを指定し、内容を出力
+     * .as以降で出力形式をしていしている
+     */
     public Result index() {
-        return new Result(
-                new ResponsiHeader(200, new HashMap<>),
-                new HttpEntity.Strict(
-                        ByteString.fromString("This is sample text."),
-                        Optional.ofNullable("text/plain")
-                )
-        );
+        return ok("<h1>Hello!</h1><p>This is sample message.<p>")
+                .as("text/html");
     }
 
 
